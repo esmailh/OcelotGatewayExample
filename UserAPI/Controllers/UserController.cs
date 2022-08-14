ebefc28bf926f52e84327eeb9e68722bf51901cb
EsmailHatami <esmailh7023@gmail.com>
2022-08-14 09:04:04 +0430

    ProductAPI
    ProductAPI

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.Data;
+using UserAPI.Models;
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private List<User> Users;
+        public UserController()
+        {
+            Users = DataModelGeneration.UserGenerate();
+        }
+        [HttpGet]
+        public IEnumerable<User> UserList()
+        {
+            var UserList = Users;
+            return UserList;
+        }
+        [HttpGet("{id}")]
+        public User GetUserById(int id)
+        {
+            return Users.SingleOrDefault(x => x.UserId == id, new User());
+        }
+        [HttpPost]
+        public User AddUser(User User)
+        {
+            Users.Add(User);
+            return User;
+        }
+        [HttpPut]
+        public User UpdateUser(User User)
+        {
+            var prod = Users.SingleOrDefault(x => x.UserId == User.UserId);
+            Users.Remove(prod);
+            Users.Add(User);
+            return User;
+        }
+        [HttpDelete("{id}")]
+        public bool DeleteUser(int id)
+        {
+            var User = Users.SingleOrDefault(p => p.UserId == id);
+            Users.Remove(User);
+            return true;
+        }
+    }
+}
---
+++ UserAPI/Data/DataModelGeneration.cs
@@ -0,0 +1,22 @@
+﻿using UserAPI.Models;
+
+namespace UserAPI.Data
+{
+    public static class DataModelGeneration
+    {
+        public static List<User> UserGenerate()
+        {
+            var output = new List<User>();
+            for (int i = 0; i < 10; i++)
+            {
+                User User = new User();
+                User.UserId = i;
+                User.UserName = $"UserName{i}";
+                User.Address = $"Address{i}";
+               
+                output.Add(User);
+            }
+            return output;
+        }
+    }
+}
